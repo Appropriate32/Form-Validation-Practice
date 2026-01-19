2221c42c0a07b0f3c983d3952b13442b45bf8605
Appropriate32 <eissadar3218@gmail.com>
2026-01-20 00:24:40 +0500

    Added basic minimum length validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./styles.css";
+
+/**
+ * @param {HTMLInputElement} emailInput
+ * @param {HTMLInputElement} countryInput
+ * @param {HTMLInputElement} Input
+ */
+
+function validateForm(Input) {
+  if (Input.validity.valueMissing) {
+    Input.setCustomValidity("Input cannot be empty");
+    Input.reportValidity();
+  }
+  if (Input.value.length < Input.minLength) {
+    Input.setCustomValidity(
+      `Length too short it should be atleast ${Input.minLength} characters!`,
+    );
+    Input.reportValidity();
+  } else {
+    Input.setCustomValidity("");
+  }
+}
+
+const emailInput = document.querySelector("#email");
+const countryInput = document.querySelector("#country");
+const postalInput = document.querySelector("#postal");
+const passwordInput = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const form = document.querySelector("form");
+
+emailInput.addEventListener("input", () => validateForm(emailInput));
+countryInput.addEventListener("input", () => validateForm(countryInput));
+postalInput.addEventListener("input", () => validateForm(postalInput));
+passwordInput.addEventListener("input", () => validateForm(passwordInput));
+confirmPassword.addEventListener("input", () => validateForm(confirmPassword));
